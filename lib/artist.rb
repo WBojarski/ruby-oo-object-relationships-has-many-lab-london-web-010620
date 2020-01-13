44bfbd47c406ab2ed1177efbbc648c8c0f4260cc
Wojciech Bojarski <bojawojt@gmail.com>
2020-01-13 08:14:47 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,10 @@
 class Artist
 
     attr_accessor :name, :songs
-
-    
-    @@songs = []
+    @songs = []
 
     def initialize(name)
         @name = name
-        
-
     end
 
     def songs 
@@ -16,16 +12,14 @@ class Artist
     end
 
     def add_song(song)
-        
         song.artist = self
-        @@songs << song
-        
+        @songs << song        
     end
 
     def add_song_by_name(name)
         song = Song.new(name)
         song.artist = self
-        @@songs << song
+        @songs << song
         
     end
 
