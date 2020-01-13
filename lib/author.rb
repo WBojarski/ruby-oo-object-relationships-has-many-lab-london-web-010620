a7845eeb55da5ec2f1121c01a9c0cc3dd6f6c894
Wojciech Bojarski <bojawojt@gmail.com>
2020-01-13 09:14:21 +0000

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -2,11 +2,12 @@ class Author
     
     attr_accessor :name, :posts
 
-    @@posts = []
+    
     
 
     def initialize(name)
         @name = name
+        @posts = []
     end
 
 
@@ -16,13 +17,13 @@ class Author
 
     def add_post(post)
         post.author = self
-        @@posts << post
+        @posts << post
     end
 
     def add_post_by_title(title)
         post = Post.new(title)
         post.author = self
-        @@posts << post
+        @posts << post
     end
 
     def self.post_count
